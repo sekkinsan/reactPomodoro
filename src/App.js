733cdde25aefcc22766e4443a862e5db19e72e6f
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-10-22 20:26:16 -0700

    started basic template for pomodoro, sectioned off

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Header extends Component {
+  render() {
+  return (
+    <h1 className="header"> Pomodoro </h1>
+    );
+  }
+}
+
+class Timer extends Component {
+  render() {
+    return (
+      <h2> Timer and stuff </h2>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <h3> Footer and stuff </h3>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <Header />
+          <Timer />
+          <Footer />
       </div>
     );
   }
